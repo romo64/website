e85cbba6904a8dc1fd855594973867685a87dee7
LDS <95876389+romo64@users.noreply.github.com>
2023-11-04 20:59:20 -0300

    Update script-img.js
    //

--- script-img.js
+++ script-img.js
@@ -7,34 +7,34 @@ function openImg(esc){
     totalImg = esc
     if(esc == 1){
         fulImgBox.style.display = 'flex';
-        fulImg.src="images/equipos/mercedes.jpg"
+        fulImg.src="/images/equipos/mercedes.jpg"
     }else if(esc==2){
         fulImgBox.style.display = 'flex';
-        fulImg.src="images/equipos/ferrari.jpg"
+        fulImg.src="/images/equipos/ferrari.jpg"
     }else if(esc==3){
         fulImgBox.style.display = 'flex';
-        fulImg.src="images/equipos/redbull.jpg"
+        fulImg.src="/images/equipos/redbull.jpg"
     }else if(esc==4){
         fulImgBox.style.display = 'flex';
-        fulImg.src="images/equipos/mclaren.jpg"
+        fulImg.src="/images/equipos/mclaren.jpg"
     }else if(esc==5){
         fulImgBox.style.display = 'flex';
-        fulImg.src="images/equipos/aston.jpg"
+        fulImg.src="/images/equipos/aston.jpg"
     }else if(esc==6){
         fulImgBox.style.display = 'flex';
-        fulImg.src="images/equipos/alpine.jpg"
+        fulImg.src="/images/equipos/alpine.jpg"
     }else if(esc==7){
         fulImgBox.style.display = 'flex';
-        fulImg.src="images/equipos/haas.jpg"
+        fulImg.src="/images/equipos/haas.jpg"
     }else if(esc==8){
         fulImgBox.style.display = 'flex';
-        fulImg.src="images/equipos/alfa.jpg"
+        fulImg.src="/images/equipos/alfa.jpg"
     }else if(esc==9){
         fulImgBox.style.display = 'flex';
-        fulImg.src="images/equipos/alpha.jpg"
+        fulImg.src="/images/equipos/alpha.jpg"
     }else if(esc==10){
         fulImgBox.style.display = 'flex';
-        fulImg.src="images/equipos/will.jpg"
+        fulImg.src="/images/equipos/will.jpg"
     }
 }
 
@@ -60,93 +60,93 @@ function funcionEjecutar(side){
 
     if(totalImg==1){
         if(foto==2){
-            fulImg.src="images/equipos/mercedes.jpg"
+            fulImg.src="/images/equipos/mercedes.jpg"
         }else if(foto==1){
-            fulImg.src="images/equipos/mercedes2.jpg"
+            fulImg.src="/images/equipos/mercedes2.jpg"
         }
         else{
-            fulImg.src="images/equipos/mercedes3.jpg"
+            fulImg.src="/images/equipos/mercedes3.jpg"
         }
     }else if(totalImg==2){
         if(foto==2){
-            fulImg.src="images/equipos/ferrari.jpg"
+            fulImg.src="/images/equipos/ferrari.jpg"
         }else if(foto==1){
-            fulImg.src="images/equipos/ferrari2.jpg"
+            fulImg.src="/images/equipos/ferrari2.jpg"
         }
         else{
-            fulImg.src="images/equipos/ferrari3.jpg"
+            fulImg.src="/images/equipos/ferrari3.jpg"
         }
     }else if(totalImg==3){
         if(foto==2){
-            fulImg.src="images/equipos/redbull.jpg"
+            fulImg.src="/images/equipos/redbull.jpg"
         }else if(foto==1){
-            fulImg.src="images/equipos/redbull2.jpg"
+            fulImg.src="/images/equipos/redbull2.jpg"
         }
         else{
-            fulImg.src="images/equipos/redbull3.jpg"
+            fulImg.src="/images/equipos/redbull3.jpg"
         }
     }else if(totalImg==4){
         if(foto==2){
-            fulImg.src="images/equipos/mclaren.jpg"
+            fulImg.src="/images/equipos/mclaren.jpg"
         }else if(foto==1){
-            fulImg.src="images/equipos/mclaren2.jpg"
+            fulImg.src="/images/equipos/mclaren2.jpg"
         }
         else{
-            fulImg.src="images/equipos/mclaren3.jpg"
+            fulImg.src="/images/equipos/mclaren3.jpg"
         }
     }else if(totalImg==5){
         if(foto==2){
-            fulImg.src="images/equipos/aston.jpg"
+            fulImg.src="/images/equipos/aston.jpg"
         }else if(foto==1){
-            fulImg.src="images/equipos/aston2.jpg"
+            fulImg.src="/images/equipos/aston2.jpg"
         }
         else{
-            fulImg.src="images/equipos/aston3.jpg"
+            fulImg.src="/images/equipos/aston3.jpg"
         }
     }else if(totalImg==6){
         if(foto==2){
-            fulImg.src="images/equipos/alpine.jpg"
+            fulImg.src="/images/equipos/alpine.jpg"
         }else if(foto==1){
-            fulImg.src="images/equipos/alpine2.jpg"
+            fulImg.src="/images/equipos/alpine2.jpg"
         }
         else{
-            fulImg.src="images/equipos/alpine3.jpg"
+            fulImg.src="/images/equipos/alpine3.jpg"
         }
     }else if(totalImg==7){
         if(foto==2){
-            fulImg.src="images/equipos/haas.jpg"
+            fulImg.src="/images/equipos/haas.jpg"
         }else if(foto==1){
-            fulImg.src="images/equipos/haas2.jpg"
+            fulImg.src="/images/equipos/haas2.jpg"
         }
         else{
-            fulImg.src="images/equipos/haas3.jpg"
+            fulImg.src="/images/equipos/haas3.jpg"
         }
     }else if(totalImg==8){
         if(foto==2){
-            fulImg.src="images/equipos/alfa.jpg"
+            fulImg.src="/images/equipos/alfa.jpg"
         }else if(foto==1){
-            fulImg.src="images/equipos/alfa2.jpg"
+            fulImg.src="/images/equipos/alfa2.jpg"
         }
         else{
-            fulImg.src="images/equipos/alfa3.jpg"
+            fulImg.src="/images/equipos/alfa3.jpg"
         }
     }else if(totalImg==9){
         if(foto==2){
-            fulImg.src="images/equipos/alpha.jpg"
+            fulImg.src="/images/equipos/alpha.jpg"
         }else if(foto==1){
-            fulImg.src="images/equipos/alpha2.jpg"
+            fulImg.src="/images/equipos/alpha2.jpg"
         }
         else{
-            fulImg.src="images/equipos/alpha3.jpg"
+            fulImg.src="/images/equipos/alpha3.jpg"
         }
     }else if(totalImg==10){
         if(foto==2){
-            fulImg.src="images/equipos/will.jpg"
+            fulImg.src="/images/equipos/will.jpg"
         }else if(foto==1){
-            fulImg.src="images/equipos/will2.jpg"
+            fulImg.src="/images/equipos/will2.jpg"
         }
         else{
-            fulImg.src="images/equipos/will3.jpg"
+            fulImg.src="/images/equipos/will3.jpg"
         }
     }
 }
